29e3e40bcc4338f7d3ceb826ddbacaa9555654db
kishor ramani <kishorkeepsmiling@gmail.com>
2021-02-26 13:42:58 +0530

    First Commit

--- app/src/main/java/com/example/androiddevchallenge/model/Pet.kt
+++ app/src/main/java/com/example/androiddevchallenge/model/Pet.kt
@@ -13,22 +13,22 @@ data class Pet(
 
 val petDataSource = listOf(
     Pet(
-        1, "Cat1", "♂", "CAT", "3 month old", R.drawable.cat1
+        1, "Mini", "♂", "CAT", "3 month old", R.drawable.cat1
     ),
     Pet(
-        2, "Cat2", "♀", "CAT", "2 year old", R.drawable.cat2
+        2, "Biladi", "♀", "CAT", "2 year old", R.drawable.cat2
     ),
     Pet(
-        3, "Dog1", "♂", "DOG","2 years old", R.drawable.dog1
+        3, "Sheru", "♂", "DOG","2 years old", R.drawable.dog1
     ),
     Pet(
-        4, "Dog2", "♂", "DOG","5 years old", R.drawable.dog2
+        4, "Rocky", "♂", "DOG","5 years old", R.drawable.dog2
     ),
     Pet(
-        5, "Cat3", "♀", "CAT", "1.7 year old", R.drawable.cat3
+        5, "Tinu", "♀", "CAT", "1.7 year old", R.drawable.cat3
     ),
     Pet(
-        6, "Dog3", "♀", "DOG","5 years old", R.drawable.dog2
+        6, "Rock", "♀", "DOG","5 years old", R.drawable.dog3
     ),
 )
 
